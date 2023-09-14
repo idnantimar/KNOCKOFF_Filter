4e5179037a5bd04971b7547948a821b5a432ecd5
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2023-09-14 23:16:50 +0530

    Update simulation2.py

--- SIMULATIONs/simulation2.py
+++ SIMULATIONs/simulation2.py
@@ -458,3 +458,297 @@ plt.show()
 
 # *****************************************************************************
 
+
+"""
+Created on Mon Sep 11 19:30:52 2023
+
+Topic: Checking whether randomization of columns actually improves sKnockOff or not.
+        All the columns in the DataMatrix are not independent here. The correlation matrix is based on some real data.
+@author: R.Nandi
+"""
+
+
+# working dir & useful Modules ..........................
+from os import chdir
+chdir("G:\PROJECTWORK_M.Stat_FINAL\SIMULATIONs\SIMULATION-4")
+
+from sys import path
+path.append("G:\PROJECTWORK_M.Stat_FINAL\CODEs")
+from KNOCKOFF_MainFile import *
+
+
+
+#### MMD based diagnostic of the quality of Knockoff ==========================
+
+from sklearn.metrics import pairwise_distances
+from random import randint
+
+
+def Dist_Z1Z2(Z1,Z2=None):
+    #  estimates E|Z1-Z2| , where Z1 & Z2 are two independent random variables
+    # Inputs are two DataMatrix corresponding to Z1 & Z2 observations respectively
+    dist = pairwise_distances(Z1,Z2)
+    np.fill_diagonal(dist ,0)
+    return np.mean(dist)
+
+
+MMD_score = lambda P1,P2 : Dist_Z1Z2(P1) + Dist_Z1Z2(P2) - 2*Dist_Z1Z2(P1,P2)
+
+
+
+
+def MMD_checkQuality(XandX_knockoff , n_partialSwap = 10):
+    """
+    let LHS = [X,X_knockoff]
+        RHS = anySwap(LHS)
+        Dist_Z1Z2 = estimated_E|Z1-Z2| , where Z1 & Z2 are two independent random variables
+
+    If the knockoff copy is of good quality, LHS & RHS should be identically distributed.
+        MMD_score = Dist_Z1Z2(LHS,LHS) + Dist_Z1Z2(RHS,RHS) - 2*Dist_Z1Z2(LHS,RHS)  , should be near 0 in such case. The further this score from 0 , it says the knockoff fails to mimic the original data.
+    """
+    X,X_knockoff = XandX_knockoff
+    n,p = X.shape
+    LHS = pd.concat([X,X_knockoff],axis=1)
+    fullSwap = lambda : pd.concat([X_knockoff,X],axis=1)
+    def partialSwap():
+        col_ix = np.array(range(2*p))
+        swappable = choice(range(p),size=randint(1,p),replace=False)
+        col_ix[swappable] += p
+        col_ix[(swappable+p)] -= p
+        return LHS.iloc[:,col_ix]
+
+    score = [MMD_score(LHS,fullSwap())]
+    for _ in range(n_partialSwap):
+        score += [MMD_score(LHS,partialSwap())]
+
+    return np.mean(score)
+
+
+
+
+# *****************************************************************************
+##
+###
+####
+###
+##
+#
+#### SIMULATION-1 =============================================================
+X = pd.read_excel("Concrete_Data.xls")
+
+n_knockoff = 12
+Data_withoutModification = genMulti(X,n_knockoff,method=sKnockOff)
+Data_withoutModification_Reverse = genMulti(X.iloc[:,::-1],n_knockoff,method=sKnockOff)
+Data_withModification = genMulti(X,n_knockoff,method=sKnockOff_Modified)
+
+DATA = {"without_randomization":Data_withoutModification,
+        "without_randomization_Reverse":Data_withoutModification_Reverse,
+        "with_randomization":Data_withModification}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Scores = lambda data, method : list(map(method,data))
+
+SCORES = pd.DataFrame({"without_randomization":Scores(Data_withoutModification,MMD_checkQuality),
+                "without_randomization_Reverse":Scores(Data_withoutModification_Reverse,MMD_checkQuality),
+                "with_randomization":Scores(Data_withModification,MMD_checkQuality)})
+plt.figure(figsize=(8,6))
+sns.boxplot(data=SCORES,palette=['red','darkgrey','green'])
+plt.axhline(color='black',linestyle='dashed')
+plt.ylabel('MMD scores')
+plt.show()
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+def pairwise_absCorrelation(XandX_knockoff):
+    """
+    Computes correlation coefficient between Xj & Xj_knockoff
+    """
+    X,X_knockoff = XandX_knockoff
+    n,p = X.shape
+    names = X.columns
+    pairCor = np.corrcoef(X,X_knockoff,rowvar=False)
+    pairCor = np.diag(pairCor[p:,:p])
+
+    return pd.Series(np.abs(pairCor),index=names)
+
+
+
+absCORR = {}
+absCORR['without_randomization'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withoutModification))).mean(axis=0)
+ix = absCORR['without_randomization'].index
+absCORR['without_randomization_Reverse'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withoutModification_Reverse))).mean(axis=0)
+absCORR['with_randomization'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withModification))).mean(axis=0)
+absCORR = pd.DataFrame(absCORR,index=ix)
+
+
+D = pd.melt(absCORR,ignore_index=False)
+D.rename(columns={'variable':'sKnockOff'},inplace=True)
+plt.figure(figsize=(7,6))
+sns.lineplot(x=D.index,y='value',hue='sKnockOff',data=D,palette=['red','darkgrey','green'])
+plt.xticks(rotation=45)
+plt.ylabel('absolute pairwise corr')
+plt.xlabel('column index ')
+plt.show()
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+Data,Data_knockoff = Data_withModification[np.random.randint(0,n_knockoff)]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | with_randomization',scale_the_corrplot=1,KDE=True)
+
+Data,Data_knockoff = Data_withoutModification[np.random.randint(0,n_knockoff)]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | without_randomization',scale_the_corrplot=1,KDE=True)
+
+Data,Data_knockoff = Data_withoutModification_Reverse[np.random.randint(0,n_knockoff)]
+Data = Data.iloc[:,::-1]
+Data_knockoff = Data_knockoff.iloc[:,::-1]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | without_randomization_Reverse',scale_the_corrplot=1,KDE=True)
+
+
+# *****************************************************************************
+##
+###
+####
+###
+##
+#
+#### SIMULATION-2 =============================================================
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+X2 = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+X2 = pd.DataFrame(X2)
+
+n_knockoff = 12
+Data_withoutModification = genMulti(X2,n_knockoff,method=sKnockOff)
+Data_withoutModification_Reverse = genMulti(X2.iloc[:,::-1],n_knockoff,method=sKnockOff)
+Data_withModification = genMulti(X2,n_knockoff,method=sKnockOff_Modified)
+
+DATA2 = {"without_randomization":Data_withoutModification,
+        "without_randomization_Reverse":Data_withoutModification_Reverse,
+        "with_randomization":Data_withModification}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Scores = lambda data, method : list(map(method,data))
+
+SCORES2 = pd.DataFrame({"without_randomization":Scores(Data_withoutModification,MMD_checkQuality),
+                "without_randomization_Reverse":Scores(Data_withoutModification_Reverse,MMD_checkQuality),
+                "with_randomization":Scores(Data_withModification,MMD_checkQuality)})
+plt.figure(figsize=(8,6))
+sns.boxplot(data=SCORES2,palette=['red','darkgrey','green'])
+plt.axhline(color='black',linestyle='dashed')
+plt.ylabel('MMD scores')
+plt.show()
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+absCORR2 = {}
+absCORR2['without_randomization'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withoutModification))).mean(axis=0)
+ix = absCORR2['without_randomization'].index
+absCORR2['without_randomization_Reverse'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withoutModification_Reverse))).mean(axis=0)
+absCORR2['with_randomization'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withModification))).mean(axis=0)
+absCORR2 = pd.DataFrame(absCORR2,index=ix)
+
+
+D = pd.melt(absCORR2,ignore_index=False)
+D.rename(columns={'variable':'sKnockOff'},inplace=True)
+plt.figure(figsize=(7,6))
+sns.lineplot(x=D.index,y='value',hue='sKnockOff',data=D,palette=['red','darkgrey','green'])
+plt.xticks(rotation=45)
+plt.ylabel('absolute pairwise corr')
+plt.xlabel('column index ')
+plt.show()
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+Data,Data_knockoff = Data_withModification[np.random.randint(0,n_knockoff)]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | with_randomization',scale_the_corrplot=0.8,KDE=True)
+
+Data,Data_knockoff = Data_withoutModification[np.random.randint(0,n_knockoff)]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | without_randomization',scale_the_corrplot=0.8,KDE=True)
+
+Data,Data_knockoff = Data_withoutModification_Reverse[np.random.randint(0,n_knockoff)]
+Data = Data.iloc[:,::-1]
+Data_knockoff = Data_knockoff.iloc[:,::-1]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | without_randomization_Reverse',scale_the_corrplot=0.8,KDE=True)
+
+
+
+# *****************************************************************************
+##
+###
+####
+###
+##
+#
+#### SIMULATION-3 =============================================================
+X3= pd.read_csv("my_data.csv",header = None)
+
+n_knockoff = 12
+Data_withoutModification = genMulti(X3,n_knockoff,method=sKnockOff)
+Data_withoutModification_Reverse = genMulti(X3.iloc[:,::-1],n_knockoff,method=sKnockOff)
+Data_withModification = genMulti(X3,n_knockoff,method=sKnockOff_Modified)
+
+DATA3 = {"without_randomization":Data_withoutModification,
+        "without_randomization_Reverse":Data_withoutModification_Reverse,
+        "with_randomization":Data_withModification}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Scores = lambda data, method : list(map(method,data))
+
+SCORES3 = pd.DataFrame({"without_randomization":Scores(Data_withoutModification,MMD_checkQuality),
+                "without_randomization_Reverse":Scores(Data_withoutModification_Reverse,MMD_checkQuality),
+                "with_randomization":Scores(Data_withModification,MMD_checkQuality)})
+plt.figure(figsize=(8,6))
+sns.boxplot(data=SCORES3,palette=['red','darkgrey','green'])
+plt.axhline(color='black',linestyle='dashed')
+plt.ylabel('MMD scores')
+plt.show()
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+absCORR3 = {}
+absCORR3['without_randomization'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withoutModification))).mean(axis=0)
+ix = absCORR3['without_randomization'].index
+absCORR3['without_randomization_Reverse'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withoutModification_Reverse))).mean(axis=0)
+absCORR3['with_randomization'] = pd.DataFrame(list(map(pairwise_absCorrelation,Data_withModification))).mean(axis=0)
+absCORR3 = pd.DataFrame(absCORR3,index=ix)
+
+
+D = pd.melt(absCORR3,ignore_index=False)
+D.rename(columns={'variable':'sKnockOff'},inplace=True)
+plt.figure(figsize=(7,6))
+sns.lineplot(x=D.index,y='value',hue='sKnockOff',data=D,palette=['red','darkgrey','green'])
+plt.xticks(rotation=60)
+plt.ylabel('absolute pairwise corr')
+plt.xlabel('column index ')
+plt.show()
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+Data,Data_knockoff = Data_withModification[np.random.randint(0,n_knockoff)]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | with_randomization',scale_the_corrplot=0.5)
+
+Data,Data_knockoff = Data_withoutModification[np.random.randint(0,n_knockoff)]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | without_randomization',scale_the_corrplot=0.5)
+
+Data,Data_knockoff = Data_withoutModification_Reverse[np.random.randint(0,n_knockoff)]
+Data = Data.iloc[:,::-1]
+Data_knockoff = Data_knockoff.iloc[:,::-1]
+Visual(Data,Data_knockoff,Cat_or_Num(Data),appendTitle=' | without_randomization_Reverse',scale_the_corrplot=0.5)
+
+
+# *****************************************************************************
+
