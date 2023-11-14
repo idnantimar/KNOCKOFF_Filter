eefd6f8a7661826215d34b3c311a2e33627621ee
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2023-11-14 05:40:42 +0530

    Update Diagnostics.py

--- KnockOff_101/Diagnostics.py
+++ KnockOff_101/Diagnostics.py
@@ -1,22 +1,17 @@
 """
 Created on Tue Sep 19 23:16:49 2023
-Topic: Checking the quality of knockoff
+Topic: Checking the quality of knockoff (For Numerical data only)
 @author: R.Nandi
 """
+
 from .Basics import *
 #### Diagnostic of the quality of knockoff ====================================
-'''
- ** NOTE: For Numerical data only
-'''
-from sklearn.metrics.pairwise import pairwise_kernels
-import random
 
 
-def _kernel_Z1Z2(Z1,Z2=None,metric_type='rbf'):
-    #  estimates E[k(Z1,Z2)] = <E[Phi(Z1)],E[Phi(Z2)]>
-    # Inputs are two DataMatrix corresponding to Z1 & Z2 observations respectively
-    D= pairwise_kernels(Z1,Z2,metric_type)
-    if Z2 is None :
+def _E_kernel_Z1Z2(D):
+    # estimates E[k(Z1,Z2)] = <E[Phi(Z1)],E[Phi(Z2)]>
+    # Input is the gram-matrix
+    if (D==D.T).all():
         np.fill_diagonal(D,0)
         n = D.shape[0]
         out = np.mean(D)*n/(n-1)
@@ -24,18 +19,37 @@ def _kernel_Z1Z2(Z1,Z2=None,metric_type='rbf'):
     return out
 
 
-_MMD_score = lambda P1,P2,kernel_type : _kernel_Z1Z2(P1,metric_type=kernel_type) + _kernel_Z1Z2(P2,metric_type=kernel_type) - 2*_kernel_Z1Z2(P1,P2,metric_type=kernel_type)
+
+from sklearn.metrics.pairwise import pairwise_distances
+from scipy.spatial.distance import euclidean,pdist,squareform
+from scipy.stats import norm as normal_distribution
+
+def _RBF_median_heuristic(Z1,Z2=None,k=1):
+    # RBF kernel with scale=k*(median of observed distances)
+    if Z2 is None : D = pdist(Z1)
+    else : D = pdist(np.vstack((Z1,Z2)))
+    sd = k*np.median(D)
+    D = squareform(D)
+    if Z2 is not None :
+        n = len(Z1)
+        D = D[:n,n:]
+    K = normal_distribution.pdf(D,scale=sd)
+    kern_map = lambda z2,z1 : normal_distribution.pdf(euclidean(z1,z2),scale=sd)
+    return [K,kern_map,sd]
 
 
 
-def MMD_checkQuality(X1st,X1st_knockoff,X2nd,X2nd_knockoff,n_partialSwap=20,set_seed=None,kernel_type='rbf'):
+import random
+from sklearn.metrics.pairwise import pairwise_kernels
+
+def MMD_checkQuality(X1st,X1st_knockoff,X2nd,X2nd_knockoff,n_partialSwap=20,set_seed=None):
     """
     let
        * LHS = [X1st,X1st_knockoff]
        * RHS = anySwap([X2nd,X2nd_knockoff])
-       * kernel_Z1Z2 = estimated_<E[Phi(Z1)],E[Phi(Z2)]> , where Z1 & Z2 are two independent random variables , Phi() some feature map
+       * E_kernel_Z1Z2 = estimated_<E[Phi(Z1)],E[Phi(Z2)]> , where Z1 & Z2 are two independent random variables , Phi() some feature map
     If the knockoff copy is of good quality, LHS & RHS should be identically distributed.
-        MMD_score = kernel_Z1Z2(LHS,LHS) + kernel_Z1Z2(RHS,RHS) - 2*kernel_Z1Z2(LHS,RHS)  , should be near 0 in such case. The further this score exceeds 0 , it says the knockoff fails to mimic the original data.
+        MMD_score = E_kernel_Z1Z2(LHS,LHS) + E_kernel_Z1Z2(RHS,RHS) - 2*E_kernel_Z1Z2(LHS,RHS)  , should be near 0 in such case. The further this score exceeds 0 , it says the knockoff fails to mimic the original data.
     (If we could actually compute the expectations E[Phi(Zi)] , we must get MMD_score>=0 . But due to sample estimates , this score can be -ve sometimes.)
     NOTE: To make the observations in LHS & RHS independent , we need -
                 * X1st & X2nd two independent DataMatrix , though the observations are identically distributed
@@ -50,11 +64,18 @@ def MMD_checkQuality(X1st,X1st_knockoff,X2nd,X2nd_knockoff,n_partialSwap=20,set_
         col_ix[swappable] += p
         col_ix[(swappable+p)] -= p
         return fullSwap_RHS.iloc[:,col_ix]
-    score = [_MMD_score(LHS,fullSwap_RHS,kernel_type)]
+    k = lambda Z1,Z2=None : pairwise_kernels(Z1,Z2,metric=_RBF_median_heuristic(LHS,fullSwap_RHS)[1])
+    score = [_E_kernel_Z1Z2(k(LHS))
+             + _E_kernel_Z1Z2(k(fullSwap_RHS))
+             - 2*_E_kernel_Z1Z2(k(LHS,fullSwap_RHS))]
     random.seed(set_seed)
     np.random.seed(set_seed)
     for _ in range(n_partialSwap):
-        score += [_MMD_score(LHS,partialSwap(),kernel_type)]
+        partialSwap_RHS = partialSwap()
+        k = lambda Z1,Z2=None : pairwise_kernels(Z1,Z2,metric=_RBF_median_heuristic(LHS,partialSwap_RHS)[1])
+        score += [_E_kernel_Z1Z2(k(LHS))
+                 + _E_kernel_Z1Z2(k(partialSwap_RHS))
+                 - 2*_E_kernel_Z1Z2(k(LHS,partialSwap_RHS))]
     return np.mean(score)
 
 
