73e6fed967157df0e919f37d91931238f9d2875a
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2023-12-11 22:05:40 +0530

    Update Basics.py

--- KnockOff_101/Basics.py
+++ KnockOff_101/Basics.py
@@ -17,15 +17,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
-
 #### Data Type ================================================================
 def Cat_or_Num(X):
     """ Determines whether a data column is Categorical or Numerical in a DataFrame
         ( Assuming the 1st entry of a column is not missing )
         Output is an array of size=number_of_columns ; True = Categorical , False = Numerical
     """
-    n_col=X.shape[1]
+    _,n_col = X.shape
     numericType = np.vectorize(lambda pos: isinstance(X.iloc[0,pos], (np.integer,np.floating)))
     return np.invert(numericType(range(n_col)))
 
