99058443dc9519a0a4dd70fc7781c6a3d17d178f
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2023-12-14 14:10:07 +0530

    Update Simulation_and_Visualization.py

--- KnockOff_101/Simulation_and_Visualization.py
+++ KnockOff_101/Simulation_and_Visualization.py
@@ -11,7 +11,9 @@ from .Basics import *
 
 #### Simulate Samples =========================================================
 
-def simulateIndependent(n_obs,col_type,NUM=lambda size: np.random.normal(0,1,size),CAT=lambda size: np.random.choice(['A','B','C','D'],size,replace=True)):
+def simulateIndependent(n_obs,col_type,
+                        NUM=lambda size,rng: rng.normal(0,1,size),CAT=lambda size,rng: rng.choice(['A','B','C','D'],size,replace=True),
+                        generator=RNG()):
     """
     A function to simulate a DataMatrix , where each column is sampled from some population , independent of each other.
 
@@ -22,10 +24,11 @@ def simulateIndependent(n_obs,col_type,NUM=lambda size: np.random.normal(0,1,siz
     col_type : tuple as (N,C) or array as [True,False,True,True,True,...]
         * If tuple, 1st element N denotes number of Numerical columns , 2nd element C denotes number of Categorical columns. When only one kind of columns are needed, input the other element as 0
         * If array as [True,False,...] , True denotes which position is for Numerical column, False denotes which position is for Categorical column.
-    NUM : a function ;  default is lambda size: np.random.normal(0,1,size)
+    NUM : a function ;  default is lambda size: normal(0,1,size)
         Mentions from where to sample Numerical columns.
-    CAT : a function ; default is lambda size: np.random.choice(['A','B','C','D'],size,replace=True))
+    CAT : a function ; default is lambda size: choice(['A','B','C','D'],size,replace=True))
         Mentions from where to sample Categorical columns.
+    generator : random number generator ; default np.random.default_rng(seed=None)
 
     Returns
     -------
@@ -38,8 +41,8 @@ def simulateIndependent(n_obs,col_type,NUM=lambda size: np.random.normal(0,1,siz
     else :
         N,C = sum(col_type),len(col_type)-sum(col_type)
 
-    NUM_Block = [pd.Series(NUM(n_obs)) for _ in range(N)]
-    CAT_Block = [pd.Series(CAT(n_obs)) for _ in range(C)]
+    NUM_Block = [pd.Series(NUM(n_obs,generator)) for _ in range(N)]
+    CAT_Block = [pd.Series(CAT(n_obs,generator)) for _ in range(C)]
     BLOCKs = pd.concat(NUM_Block+CAT_Block,axis=1)
     BLOCKs.columns = [('Num'+str(i)) for i in range(N)] + [('Cat'+str(j)) for j in range(C)]
     col_order = []
@@ -60,7 +63,8 @@ def simulateIndependent(n_obs,col_type,NUM=lambda size: np.random.normal(0,1,siz
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-def simulateJoint(n_obs,popln=lambda size: np.random.multivariate_normal(np.random.rand(10), np.corrcoef(np.random.rand(10,100)),size)):
+def simulateJoint(n_obs,popln=lambda size_,rng: rng.multivariate_normal(rng.normal(size=10), np.corrcoef(rng.random(size=(10,100))),size=size_),
+                  generator=RNG()):
     """
     A function to simulate a DataMatrix from a joint-distribution specified.
 
@@ -68,7 +72,8 @@ def simulateJoint(n_obs,popln=lambda size: np.random.multivariate_normal(np.rand
     ----------
     n_obs : int
         Number of observations per column.
-    popln : a function mentioning the joint distribution of the columns ; default is lambda size: np.random.multivariate_normal(np.random.rand(10), np.corrcoef(np.random.rand(10,100)),size)
+    popln : a function mentioning the joint distribution of the columns ; default is lambda size: multivariate_normal(normal(10), np.corrcoef(random(10,100)),size)
+    generator : random number generator ; default np.random.default_rng(seed=None)
 
     Returns
     -------
@@ -76,10 +81,67 @@ def simulateJoint(n_obs,popln=lambda size: np.random.multivariate_normal(np.rand
 
     """
 
-    OUT = pd.DataFrame(popln(n_obs))
+    OUT = pd.DataFrame(popln(n_obs,generator))
     OUT.columns = ['col_'+str(i) for i in OUT.columns]
 
     return OUT
+
+
+
+
+# *****************************************************************************
+##
+###
+####
+###
+##
+# Simulate Response|observed_Predictor ========================================
+
+from tensorflow.nn import softmax as logOdds_to_probability
+from os import environ as _environ
+_environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from string import ascii_uppercase
+
+
+
+def Simulate_Categorical(X=simulateIndependent(5,(2,0)),
+                         Phi= lambda x: x, return_FeatureMap=False,
+                         Beta_coeffs = np.random.randn(3,2),
+                         categories = list(ascii_uppercase),
+                         generator = RNG()):
+    """
+    A function to simulate Categorical Response based on Multinomial conditional probability & an observed DataMatrix.
+
+    Parameters
+    ----------
+    X : DataFrame or 2D-array ; size=(n,p)
+        The observed Predictors.
+    Phi : a function that takes an 1D-array as input , returns an 1D-array
+        The FeatureMap to be applied on each rows of X. Phi(X) has size=(n,m). Default is Linear FeatureMap.
+    return_FeatureMap : bool ; default False
+        Whether Phi(X) will be returned(True) or not(False) at the end.
+    Beta_coeffs : 2D-array ; size=(k,m)
+        The coefficients to be used to model the Multinomial probabilities. Each row corresponds to a response category.
+        Setting all the coeficients to 0 results in independence of Y & X.
+    categories : list or 1D-array ; length=k
+        The list of response category names.
+    generator : random number generator ; default np.random.default_rng(seed=None)
+
+    Returns
+    -------
+    Series ; Contains the name of the simulated category at every index.
+
+    """
+    X = pd.DataFrame(X).copy()
+    X = X.apply(Phi,axis=1,result_type='expand')
+    XBeta = np.matmul(X,Beta_coeffs.T)
+    Probs = logOdds_to_probability(XBeta).numpy()
+    Y = generator.multinomial(1,Probs).argmax(axis=1)
+    Y = pd.Series(Y,index=X.index)
+    Y = Y.apply(lambda i: categories[i])
+
+    return Y if not return_FeatureMap else (Y,X)
+
 
 
 
