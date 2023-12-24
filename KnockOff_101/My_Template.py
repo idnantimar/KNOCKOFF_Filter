2fefe74bc418523ce2266ed8fd31cce93f09e708
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2023-12-25 00:37:36 +0530

    Update My_Template.py

--- KnockOff_101/My_Template.py
+++ KnockOff_101/My_Template.py
@@ -19,12 +19,47 @@ from scipy.stats import rankdata
 
 
 class My_Template_FeatureImportance(SelectorMixin,BaseEstimator):
+    """
+        A common template for all the feature-importance techniques under this projectwork.
+
+        Attributes
+        ----------
+        threshold : float , default 0
+            A cut-off, any feature with importance exceeding this value will be selected,
+            otherwise will be rejected.
+
+        max_features : int , default None
+            The maximum possible number of selection. None implies no constrain,
+            otherwise the 'threshold' will be updated automatically if it attempts to
+            select more than 'max_features'.
+    """
     threshold = 0
     max_features = None
     def __init__(self,random_state=None):
         self.random_state = random_state
 
     def fit(self,X,y):
+        """
+        This is a basic fit method , computing two atributes 'n_features_in_' and 'feature_names_in_'.
+
+        (Override this with the actual implementation of required feature-importance technique ,
+        that computes the attribute 'feature_importances_')
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            The training input samples.
+
+        y : array-like of shape (n_samples,)
+            The target values .
+
+        Returns
+        -------
+        X_ : A copy of X
+
+        y_ : A copy of y
+
+        """
         X_,y_ = X.copy(),y.copy()
                 ## to prevent any accidental change in original data
         if hasattr(self,'feature_importances_') :
@@ -35,6 +70,15 @@ class My_Template_FeatureImportance(SelectorMixin,BaseEstimator):
         return X_,y_
 
     def _get_support_mask(self):
+        """
+        Get the boolean mask indicating which features are selected
+
+        Returns
+        -------
+        support : boolean array of shape (n_features,)
+            An element is True iff its corresponding feature is selected for
+            retention.
+        """
         check_is_fitted(self,attributes="feature_importances_",
                         msg="The %(name)s instance must have a 'feature_importances_' attribute.")
 
@@ -56,6 +100,17 @@ class My_Template_FeatureImportance(SelectorMixin,BaseEstimator):
 
     def plot(self,sort=False,kind='bar',ax=None,
              xlabel='features',ylabel='importance',title=None,**kwargs):
+        """
+        Make plot of 'feature_importances_'.
+
+        Parameters
+        ----------
+        sort : bool, default False
+            Whether to sort the features according to 'feature_importances_' before plotting.
+
+        kind, ax, xlabel, ylabel, title, **kwargs : keyword arguments to pass to matplotlib plotting method.
+
+        """
         check_is_fitted(self,attributes="feature_importances_",all_or_any=all,
                         msg="The %(name)s instance is missing a 'feature_importances_' or 'thresholds_' attribute.")
         if self.feature_names_in_ is None :
